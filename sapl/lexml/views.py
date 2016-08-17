3d1ed05a85c14f0f36e549540018c7d3471546f1
Edward Ribeiro <eribeiro@interlegis.leg.br>
2016-08-17 15:14:59 -0300

    Montagem da infra do LexML (em andamento)

--- sapl/lexml/views.py
+++ sapl/lexml/views.py
@@ -2,5 +2,16 @@ from sapl.crud.base import Crud
 
 from .models import LexmlProvedor, LexmlPublicador
 
-LexmlProvedorCrud = Crud.build(LexmlProvedor, 'lexml_provedor')
-LexmlPublicadorCrud = Crud.build(LexmlPublicador, 'lexml_publicador')
+# LexmlProvedorCrud = Crud.build(LexmlProvedor, 'lexml_provedor')
+# LexmlPublicadorCrud = Crud.build(LexmlPublicador, 'lexml_publicador')
+
+from django.shortcuts import render
+
+
+def add_oai_server(request):
+    template = 'lexml/add_SAPLOAIServer.html'
+    return render(request, template, {})
+
+def edit_oai_server(request):
+    template = 'lexml/edit_SAPLOAIServer.html'
+    return render(request, template, {})
