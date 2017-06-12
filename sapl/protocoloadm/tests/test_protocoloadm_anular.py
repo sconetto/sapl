a3862bae5ca8ceaf4080cf1c3e1656d134779348
Eliseu Egewarth <eliseuegewarth@gmail.com>
2017-06-12 15:52:11 -0300

    Cria arquivo test_protocoloadm_anular.py 	move testes de anulação de protocolo
    Signed-off-by: Eliseu Egewarth <eliseuegewarth@gmail.com>

---
+++ sapl/protocoloadm/tests/test_protocoloadm_anular.py
@@ -0,0 +1,113 @@
+import datetime
+
+import pytest
+from django.core.urlresolvers import reverse
+from django.utils.encoding import force_text
+from django.utils.translation import ugettext_lazy as _
+from model_mommy import mommy
+
+from sapl.materia.models import UnidadeTramitacao
+from sapl.protocoloadm.forms import AnularProcoloAdmForm
+from sapl.protocoloadm.models import (DocumentoAdministrativo, Protocolo,
+                                      StatusTramitacaoAdministrativo,
+                                      TipoDocumentoAdministrativo,
+                                      TramitacaoAdministrativo)
+
+
+@pytest.mark.django_db(transaction=False)
+def test_anular_protocolo_acessivel(admin_client):
+    response = admin_client.get(reverse('sapl.protocoloadm:anular_protocolo'))
+    assert response.status_code == 200
+
+
+@pytest.mark.django_db(transaction=False)
+def test_anular_protocolo_submit(admin_client):
+    mommy.make(Protocolo, numero='76', ano='2016', anulado=False)
+
+    # TODO: setar usuario e IP
+    response = admin_client.post(reverse('sapl.protocoloadm:anular_protocolo'),
+                                 {'numero': '76',
+                                  'ano': '2016',
+                                  'justificativa_anulacao': 'TESTE',
+                                  'salvar': 'Anular'},
+                                 follow=True)
+
+    assert response.status_code == 200
+
+    protocolo = Protocolo.objects.first()
+    assert protocolo.numero == 76
+    assert protocolo.ano == 2016
+
+    if not protocolo.anulado:
+        pytest.fail(_("Protocolo deveria estar anulado"))
+    assert protocolo.justificativa_anulacao == 'TESTE'
+
+
+@pytest.mark.django_db(transaction=False)
+def test_form_anular_protocolo_inexistente():
+    form = AnularProcoloAdmForm({'numero': '1',
+                                 'ano': '2016',
+                                 'justificativa_anulacao': 'TESTE'})
+
+    # Não usa o assert form.is_valid() == False por causa do PEP8
+    if form.is_valid():
+        pytest.fail(_("Form deve ser inválido"))
+    assert form.errors['__all__'] == [_("Protocolo 1/2016 não existe")]
+
+
+@pytest.mark.django_db(transaction=False)
+def test_form_anular_protocolo_valido():
+    mommy.make(Protocolo, numero='1', ano='2016', anulado=False)
+    form = AnularProcoloAdmForm({'numero': '1',
+                                 'ano': '2016',
+                                 'justificativa_anulacao': 'TESTE'})
+    if not form.is_valid():
+        pytest.fail(_("Form deve ser válido"))
+
+
+@pytest.mark.django_db(transaction=False)
+def test_form_anular_protocolo_anulado():
+    mommy.make(Protocolo, numero='1', ano='2016', anulado=True)
+    form = AnularProcoloAdmForm({'numero': '1',
+                                 'ano': '2016',
+                                 'justificativa_anulacao': 'TESTE'})
+    assert form.errors['__all__'] == \
+        [_("Protocolo 1/2016 já encontra-se anulado")]
+
+
+@pytest.mark.django_db(transaction=False)
+def test_form_anular_protocolo_campos_obrigatorios():
+    mommy.make(Protocolo, numero='1', ano='2016', anulado=False)
+
+    # TODO: generalizar para diminuir o tamanho deste método
+
+    # numero ausente
+    form = AnularProcoloAdmForm({'numero': '',
+                                 'ano': '2016',
+                                 'justificativa_anulacao': 'TESTE'})
+    if form.is_valid():
+        pytest.fail(_("Form deve ser inválido"))
+
+    assert len(form.errors) == 1
+    assert form.errors['numero'] == [_('Este campo é obrigatório.')]
+
+    # ano ausente
+    form = AnularProcoloAdmForm({'numero': '1',
+                                 'ano': '',
+                                 'justificativa_anulacao': 'TESTE'})
+    if form.is_valid():
+        pytest.fail(_("Form deve ser inválido"))
+
+    assert len(form.errors) == 1
+    assert form.errors['ano'] == [_('Este campo é obrigatório.')]
+
+    # justificativa_anulacao ausente
+    form = AnularProcoloAdmForm({'numero': '1',
+                                 'ano': '2016',
+                                 'justificativa_anulacao': ''})
+    if form.is_valid():
+        pytest.fail(_("Form deve ser inválido"))
+
+    assert len(form.errors) == 1
+    assert form.errors['justificativa_anulacao'] == \
+                      [_('Este campo é obrigatório.')]
